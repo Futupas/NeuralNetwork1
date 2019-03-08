1cf170b8ebd5484497121bc7229171625f270e0b
Olexandr Panov <futupas@gmail.com>
2019-03-08 13:51:07 +0200

    testing refs

--- NeuralNetwork/NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork.cs
@@ -71,11 +71,11 @@ namespace NeuralNetwork
             double actual_output = this.GetResult(inputs);
             // output neuron
             Neuron out_neuron = Network[Network.Length - 1][0];
-            out_neuron.weights_delta = out_neuron.Value * (1 - out_neuron.Value) * (expected_output - out_neuron.Value);
+            Network[Network.Length - 1][0].weights_delta = out_neuron.Value * (1 - out_neuron.Value) * (expected_output - out_neuron.Value);
             for (int i = 0; i < out_neuron.Weights.Length; i++)
             {
                 double neuron_out = Network[Network.Length - 2][i].Value;
-                out_neuron.Weights[i] += learning_rate * out_neuron.weights_delta * neuron_out;
+                Network[Network.Length - 1][0].Weights[i] += learning_rate * out_neuron.weights_delta * neuron_out;
             }
             // hidden layers
             for (int i = Network.Length-2; i >= 1; i--) // от предпоследнего до второго (первого скрытого)
@@ -86,6 +86,7 @@ namespace NeuralNetwork
                     double weights_delta = 0;
                     for (int ej = 0; ej < Network[i+1].Length; ej++) // for each neuron in next layer
                     {
+                        if (Network[i + 1][ej].is_bais) continue;
                         weights_delta += (Network[i + 1][ej].weights_delta * Network[i + 1][ej].Weights[j]);
                     }
                     Network[i][j].weights_delta = weights_delta;
@@ -106,61 +107,7 @@ namespace NeuralNetwork
             return sigmoid_x * (1 - sigmoid_x);
         }
 
-        //public void Teach(double[] inputs, double expected_output)
-        //{
-        //    double actual_output = this.GetResult(inputs);
-        //    // output neuron
-        //    double error = actual_output - expected_output;
-        //    Neuron last_neuron = Network[Network.Length - 1][0];
-        //    double weights_delta = error * Derivative(last_neuron.Value);
-        //    for (int i = 0; i < last_neuron.Weights.Length; i++)
-        //    {
-        //        double weight_i = last_neuron.Weights[i];
-        //        double output_i = Network[Network.Length - 2][i].Value;
-
-        //        Network[Network.Length - 1][0].Weights[i] = weight_i - output_i * weights_delta * learning_rate;
-        //    }
-        //    //other neurons
-        //    for (int i = Network.Length - 2; i >= 0; i--) // for each layer
-        //    {
-        //        for (int j = 0; j < Network[i].Length; j++) // for each neuron in layer
-        //        {
-        //            if (Network[i][j].is_bais) continue; // continue if the neuron is bais
-        //            error = 0;
-        //            for (var j1 = 0; j1 < Network[i + 1].Length; j1++) // for each neuron in next layer
-        //            {
-        //                double current_weight = Network[i + 1][j1].Weights[j];
-        //                error += (weights_delta * current_weight);
-        //            }
-
-        //        }
-        //    }
-
-        //}
-
-        //public void TeachRecursive(int layer, int neuron)
-        //{
-        //    if (layer <= 0) return;
-        //    Neuron current_neuron = Network[layer][neuron];
-        //    if (current_neuron.is_bais) return;
-
-        //    double error = 0;
-
-
-
-        //    double weights_delta = error * Derivative(current_neuron.Value);
-
-
-
-
-        //    for (int i = 0; i < last_neuron.Weights.Length; i++)
-        //    {
-        //        double weight_i = last_neuron.Weights[i];
-        //        double output_i = Network[Network.Length - 2][i].Value;
-
-        //        Network[Network.Length - 1][0].Weights[i] = weight_i - output_i * weights_delta * learning_rate;
-        //    }
-        //}
+        
     }
 
     class Neuron
@@ -193,17 +140,25 @@ namespace NeuralNetwork
 
         public Neuron(bool is_bais, double[] input_weights)
         {
-            this.Weights = input_weights;
             this.is_bais = is_bais;
+            this.Weights = this.is_bais ? new double[0] : input_weights;
         }
         public Neuron(bool is_bais, int input_weights_count)
         {
-            this.Weights = new double[input_weights_count];
-            for (int i = 0; i < Weights.Length; i++)
-            {
-                this.Weights[i] = (new Random().NextDouble()) / 2 + .25;
-            }
             this.is_bais = is_bais;
+            if (this.is_bais)
+            {
+                this.Weights = new double[0];
+            }
+            else
+            {
+                this.Weights = new double[input_weights_count];
+                for (int i = 0; i < Weights.Length; i++)
+                {
+                    this.Weights[i] = (new Random().NextDouble()) / 2 + .25;
+                }
+            }
+            
         }
     }
 }
--- NeuralNetwork/NeuralNetwork/Program.cs
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -4,26 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//http://www.aiportal.ru/articles/neural-networks/back-propagation.html
 namespace NeuralNetwork
 {
+    class NT
+    {
+        public int A;
+        public NT(int a)
+        {
+            this.A = a;
+        }
+        public static explicit operator NT(int value)
+        {
+            return new NT(value);
+        }
+        public override string ToString()
+        {
+            return this.A.ToString();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            NT[] ints = new NT[] { (NT)0, (NT)1, (NT)2, (NT)3, (NT)4, (NT)5, (NT)6, (NT)7 };
+            foreach (NT item in ints)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine("");
+            int a = ints[2].A;
+            a = 10;
+            ints[3] = (NT)10;
+            foreach (NT item in ints)
+            {
+
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("---");
+
+
+
+
             NN ntw = new NN(
-                layers_count: 1, 
+                layers_count: 2, 
                 neurons_count: 4, 
                 inputs_count: 2, 
-                learning_rate: .8);
+                learning_rate: .1);
 
-            //ntw.Teach(new double[] { 0, 0 }, 0);
-            //ntw.Teach(new double[] { 0, 0 }, 0);
-            //ntw.Teach(new double[] { 0, 1 }, 1);
-            //ntw.Teach(new double[] { 0, 1 }, 1);
-            //ntw.Teach(new double[] { 1, 0 }, 1);
-            //ntw.Teach(new double[] { 1, 0 }, 1);
-            //ntw.Teach(new double[] { 1, 1 }, 0);
-            //ntw.Teach(new double[] { 1, 1 }, 0);
 
             ////double res = ntw.GetResult(new double[] { 0, 0 });
             //Console.WriteLine(ntw.GetResult(new double[] { .1, .1 }));
@@ -32,12 +61,43 @@ namespace NeuralNetwork
             //Console.WriteLine(ntw.GetResult(new double[] { .9, .9 }));
 
 
-            Console.WriteLine(ntw.GetResult(new double[] { .55, .1 }));
-            for (int i = 0; i < 100; i++)
-            {
-                ntw.Teach(new double[] { .55, .1 }, .4);
-                Console.WriteLine(ntw.GetResult(new double[] { .55, .1 }));
+            Console.WriteLine(ntw.GetResult(new double[] { 0, 0 }));
+            Console.WriteLine(ntw.GetResult(new double[] { 0, 1 }));
+            Console.WriteLine(ntw.GetResult(new double[] { 1, 0 }));
+            Console.WriteLine(ntw.GetResult(new double[] { 1, 1 }));
+
+            //for (int j = 0; j < 100; j++)
+            //{
+                for (int i = 0; i < 100; i++)
+                {
+                    ntw.Teach(new double[] { 0, 0 }, 0);
+                    ntw.Teach(new double[] { 0, 1 }, 1);
+                    ntw.Teach(new double[] { 1, 0 }, 1);
+                    ntw.Teach(new double[] { 1, 1 }, 0);
             }
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        ntw.Teach(new double[] { 0, 1 }, 1);
+            //    }
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        ntw.Teach(new double[] { 1, 0 }, 1);
+            //    }
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        ntw.Teach(new double[] { 1, 1 }, 0);
+            //    }
+            //}
+
+            Console.WriteLine("--");
+
+            Console.WriteLine(ntw.GetResult(new double[] { 0, 0 }));
+            Console.WriteLine(ntw.GetResult(new double[] { 0, 1 }));
+            Console.WriteLine(ntw.GetResult(new double[] { 1, 0 }));
+            Console.WriteLine(ntw.GetResult(new double[] { 1, 1 }));
+
+
+
             Console.ReadLine();
         }
     }
