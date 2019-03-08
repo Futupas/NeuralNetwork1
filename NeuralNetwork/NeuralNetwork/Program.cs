66f000fd8244b6221b17a7a862b7bdab138836d2
Olexandr Panov <futupas@gmail.com>
2019-03-09 01:16:18 +0200

    well testing, add newtonsoftJson (not using)

--- NeuralNetwork/NeuralNetwork/Program.cs
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 //http://www.aiportal.ru/articles/neural-networks/back-propagation.html
 namespace NeuralNetwork
@@ -11,74 +12,111 @@ namespace NeuralNetwork
     {
         static void Main(string[] args)
         {
-            int[] ints = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
-            foreach (int item in ints)
-            {
-                Console.Write($"{item} ");
-            }
-            Console.WriteLine("");
-            ref int a = ref ints[2];
-            a = 10;
-            foreach (int item in ints)
-            {
-                Console.Write($"{item} ");
-            }
-            Console.WriteLine("");
-            Console.WriteLine("---");
-
-
-
-
+            Console.Title = "";
             NN ntw = new NN(
                 layers_count: 1, 
                 neurons_count: 4, 
                 inputs_count: 2, 
-                learning_rate: .1);
+                learning_rate: .01);
 
+            double[] vals = new double[4] { 0, 0, 0, 0 };
+            double[] oldvals = new double[4];// = vals;
 
-            ////double res = ntw.GetResult(new double[] { 0, 0 });
-            //Console.WriteLine(ntw.GetResult(new double[] { .1, .1 }));
-            //Console.WriteLine(ntw.GetResult(new double[] { .1, .9 }));
-            //Console.WriteLine(ntw.GetResult(new double[] { .9, .1 }));
-            //Console.WriteLine(ntw.GetResult(new double[] { .9, .9 }));
+            vals[0] = ntw.GetResult(new double[] { 0, 0 });
+            Console.WriteLine(vals[0]);
+            vals[1] = ntw.GetResult(new double[] { 0, 1 });
+            Console.WriteLine(vals[1]);
+            vals[2] = ntw.GetResult(new double[] { 1, 0 });
+            Console.WriteLine(vals[2]);
+            vals[3] = ntw.GetResult(new double[] { 1, 1 });
+            Console.WriteLine(vals[3]);
 
+            oldvals[0] = vals[0];
+            oldvals[1] = vals[1];
+            oldvals[2] = vals[2];
+            oldvals[3] = vals[3];
 
-            Console.WriteLine(ntw.GetResult(new double[] { 0, 0 }));
-            Console.WriteLine(ntw.GetResult(new double[] { 0, 1 }));
-            Console.WriteLine(ntw.GetResult(new double[] { 1, 0 }));
-            Console.WriteLine(ntw.GetResult(new double[] { 1, 1 }));
-
-            //for (int j = 0; j < 100; j++)
-            //{
-            for (int i = 0; i < 10000; i++)
-                {
+            for (int i = 0; i < 1000000; i++)
+            {
                 ntw.Teach(new double[] { 0, 0 }, 0);
                 ntw.Teach(new double[] { 0, 1 }, 1);
                 ntw.Teach(new double[] { 1, 0 }, 1);
-                ntw.Teach(new double[] { 1, 1 }, 0);
+                //ntw.Teach(new double[] { 1, 1 }, 0);
+
+                vals[0] = ntw.GetResult(new double[] { 0, 0 });
+                vals[1] = ntw.GetResult(new double[] { 0, 1 });
+                vals[2] = ntw.GetResult(new double[] { 1, 0 });
+                vals[3] = ntw.GetResult(new double[] { 1, 1 });
+
+                //
+                string title = "";
+                //title += (vals[0] == oldvals[0] ? "E" : vals[0] < oldvals[0] ? "D" : "U") + "  ";
+                //title += (vals[1] == oldvals[1] ? "E" : vals[1] < oldvals[1] ? "D" : "U") + "  ";
+                //title += (vals[2] == oldvals[2] ? "E" : vals[2] < oldvals[2] ? "D" : "U") + "  ";
+                //title += (vals[3] == oldvals[3] ? "E" : vals[3] < oldvals[3] ? "D" : "U") + "  ";
+                title += (vals[0] == oldvals[0] ? "-" : vals[0] < oldvals[0] ? "↓" : "↑") + "  ";
+                title += (vals[1] == oldvals[1] ? "-" : vals[1] < oldvals[1] ? "↓" : "↑") + "  ";
+                title += (vals[2] == oldvals[2] ? "-" : vals[2] < oldvals[2] ? "↓" : "↑") + "  ";
+                title += (vals[3] == oldvals[3] ? "-" : vals[3] < oldvals[3] ? "↓" : "↑") + "  ";
+                title += "          ";
+                title += Math.Round(vals[0], 3) + "  ";
+                title += Math.Round(vals[1], 3) + "  ";
+                title += Math.Round(vals[2], 3) + "  ";
+                title += Math.Round(vals[3], 3) + "  ";
+                Console.Title = title;
+
+                oldvals[0] = vals[0];
+                oldvals[1] = vals[1];
+                oldvals[2] = vals[2];
+                oldvals[3] = vals[3];
+
+
+                Console.Write(i);
+                Console.Write("    ");
+                Console.Write(vals[0]);
+                Console.Write("    ");
+                Console.Write(vals[1]);
+                Console.Write("    ");
+                Console.Write(vals[2]);
+                Console.Write("    ");
+                Console.Write(vals[3]);
+                Console.WriteLine("");
+
+
+                //Console.Write("    ");
+                //Console.Write(ntw.GetResult(new double[] { 0, 0 }));
+                //Console.Write("    ");
+                //Console.Write(ntw.GetResult(new double[] { 0, 1 }));
+                //Console.Write("    ");
+                //Console.Write(ntw.GetResult(new double[] { 1, 0 }));
+                //Console.Write("    ");
+                //Console.Write(ntw.GetResult(new double[] { 1, 1 }));
+                //Console.WriteLine("");
             }
-            //    for (int i = 0; i < 100; i++)
-            //    {
-            //        ntw.Teach(new double[] { 0, 1 }, 1);
-            //    }
-            //    for (int i = 0; i < 100; i++)
-            //    {
-            //        ntw.Teach(new double[] { 1, 0 }, 1);
-            //    }
-            //    for (int i = 0; i < 100; i++)
-            //    {
-            //        ntw.Teach(new double[] { 1, 1 }, 0);
-            //    }
-            //}
 
             Console.WriteLine("--");
-
-            Console.WriteLine(ntw.GetResult(new double[] { 0, 0 }));
-            Console.WriteLine(ntw.GetResult(new double[] { 0, 1 }));
-            Console.WriteLine(ntw.GetResult(new double[] { 1, 0 }));
-            Console.WriteLine(ntw.GetResult(new double[] { 1, 1 }));
+            
 
 
+            //Console.WriteLine(ntw.GetResult(new double[] { 0.54, 0.2 }));
+            //Console.WriteLine(ntw.GetResult(new double[] { 0, 1 }));
+            //Console.WriteLine(ntw.GetResult(new double[] { 1, 0 }));
+            //Console.WriteLine(ntw.GetResult(new double[] { 1, 1 }));
+
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    ntw.Teach(new double[] { 0, 0 }, 0);
+            //    ntw.Teach(new double[] { 0, 1 }, 1);
+            //    ntw.Teach(new double[] { 1, 0 }, 1);
+            //    ntw.Teach(new double[] { 1, 1 }, 0);
+            //}
+
+            //Console.WriteLine("--");
+
+            //Console.WriteLine(ntw.GetResult(new double[] { 0, 0 }));
+            //Console.WriteLine(ntw.GetResult(new double[] { 0, 1 }));
+            //Console.WriteLine(ntw.GetResult(new double[] { 1, 0 }));
+            //Console.WriteLine(ntw.GetResult(new double[] { 1, 1 }));
 
             Console.ReadLine();
         }
